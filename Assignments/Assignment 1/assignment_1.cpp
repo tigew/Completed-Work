d0b13802e247afa069593005ed519ee172dc0213
Travis Peebles <cheadge@gmail.com>
2014-09-03 10:47:00 -0700

    Initial Commit
    First Assignment Skeleton done

---
+++ Assignments/Assignment 1/assignment_1.cpp
@@ -0,0 +1,123 @@
+/*
+ * Name        : assignment_1.cpp
+ * Author      : Travis Peebles
+ * Description : The purpose of this assignment is to practice basic programming
+ *               techniques using the C++ programming language.
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::stringstream;
+using std::ios_base;
+using std::boolalpha;
+
+// Function Prototypes (DO NOT ALTER)
+void CountCharacters(string &input, int &is_alpha, int &is_numeric);
+string UpAndDown(string input);
+int CountWords(string input);
+int Average(int num_array[], int limit);
+int Minimum(int num_array[], int limit);
+int Maximum(int num_array[], int limit);
+
+// Program Execution Starts Here
+int main() {
+  // This ends program execution
+  return 0;
+}
+
+/*
+ * This function will count the number of alphabetic characters and the 
+ * number of numeric characters in a string.
+ * @uses
+ * @param string &input - The input string to itterate
+ * @param int &is_alpha - The store of alpha chars
+ * @param int &is_numeric - The store of numeric chars
+ * @return Nothing 
+ */
+void CountCharacters(string &input, int &is_alpha, int &is_numeric) {
+  cout << is_alpha;
+  cout << input;
+  cout << is_numeric;
+}
+
+/*
+ * This function will take a string and return a version of it where the 
+ * first letter is capitalized, the second is lowercase, the third is 
+ * capitalized, etc.
+ * @uses 
+ * @param string input - The input String
+ * @return string - Returns the input string with the first Letter capital
+ *                  second letter lowercase, third capital and so on.
+ */
+string UpAndDown(string input) {
+  cout << input;
+  return "";
+}
+
+/* 
+ * This function will take a string of words, delimited by a space 
+ * character, and count how many words there are. The string will 
+ * never have multiple spaces between words or a space at the
+ * beginning or the end.
+ * @uses 
+ * @param string input - The string of words to be counted
+ * @return int - The number of words that have been counted from the 
+ *                     string
+ */
+int CountWords(string input) {
+  cout << input;
+  return 0;
+}
+
+/* 
+ * This function will compute the mean average of the values in the 
+ * array. The array will always be at least of size 1.
+ * @uses
+ * @param int num_array[] - The Array of which the average will be computed
+ * @param int limit - the size of the array
+ * @return int - The average of the values in the array.
+ */
+int Average(int num_array[], int limit) {
+  cout << num_array[0];
+  cout << limit;
+  return 0;
+}
+
+/*
+ * This function will find and return the smallest value in an array. The 
+ * array will always be at least of size 1.
+ * @uses
+ * @param int num_array[] - The array of which the smallest value will be found
+ * @param int limit - the size of the array
+ * @return int - The samllest number contained in the array
+ */
+int Minimum(int num_array[], int limit) {
+  cout << num_array[0];
+  cout << limit;
+  return 0;
+}
+
+/*
+ * This function will find and return the largest value in an array. The 
+ * array will always be at least of size 1.
+ * @uses
+ * @param int num_array[] - The array of which the largest value will be found
+ * @param int limit - the size of the array
+ * @return int - The largest number contained in the array
+ */
+int Maximum(int num_array[], int limit) {
+  cout << num_array[0];
+  cout << limit;
+  return 0;
+}
+
+
+
