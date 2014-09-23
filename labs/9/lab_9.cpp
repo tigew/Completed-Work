7e3cf05148fab0f3d28171ee56bfecf0e219e1cd
Travis Peebles <cheadge@gmail.com>
2014-09-23 16:02:42 -0700

    Lab 9 Finished

--- labs/9/lab_9.cpp
+++ labs/9/lab_9.cpp
@@ -108,35 +108,50 @@ int main() {
 // CODE HERE -- CLASS DEFINITION
 
 void Spaceship::set_name(string name) {
+  name_ = name;
 }
 
 void Spaceship::set_top_speed(double top_speed) {
+  top_speed_ = top_speed;
 }
 
 void Spaceship::set_fuel_source(string fuel_source) {
+  fuel_source_ = fuel_source;
 }
 
 void Spaceship::set_crew_capacity(int crew_capacity) {
+  crew_capacity_ = crew_capacity;
 }
 
 string Spaceship::name() const {
-  return "abc";
+  return name_;
 }
 
 double Spaceship::top_speed() const {
-  return 0.0;
+  return top_speed_;
 }
 
 string Spaceship::fuel_source() const {
-  return "abc";
+  return fuel_source_;
 }
 
 int Spaceship::crew_capacity() const {
-  return 0;
+  return crew_capacity_;
 }
 
 string Spaceship::ToString() const {
-  return "yes";
+  stringstream ss;
+  string string_return;
+  ss.setf(std::ios::fixed|std::ios::showpoint);
+  ss.precision(2);
+
+  ss << name_  << "\n"
+     << "Top Speed:     Warp " << top_speed_     << endl
+     << "Fuel Source:   "      << fuel_source_   << endl
+     << "Crew Capacity: "      << crew_capacity_;
+
+  string_return = ss.str();
+  return string_return;
 }
 
 // For testing (DO NOT ALTER)
