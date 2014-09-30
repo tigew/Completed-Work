8c283aff1478b1fd9ecdb25455ed25513786cdda
Travis Peebles <cheadge@gmail.com>
2014-09-30 11:43:44 -0700

    laid out template of cpp for money
    == working
    + working
    fix others

--- labs/11/money.cpp
+++ labs/11/money.cpp
@@ -21,3 +21,52 @@ const Money operator +(const Money& amount1, const Money& amount2) {
 }
 
 // CLASS FUNCTION DEFINITIONS GO HERE
+
+const Money operator -(const Money& amount1, const Money& amount2) {
+  // get cents from both objects
+  int cents_1 = amount1.cents_ + amount1.dollars_ * 100;
+  int cents_2 = amount2.cents_ + amount2.dollars_ * 100;
+  // subtract them
+  int sum_all_cents = cents_1 - cents_2;
+  // handle negatives
+  int abs_all_cents = abs(sum_all_cents);
+  int final_dollars = abs_all_cents / 100;
+  int final_cents = abs_all_cents % 100;
+  if (sum_all_cents < 0) {
+    final_dollars -= final_dollars;
+    final_cents   -= final_cents;
+  }
+  // return
+  return Money(final_dollars, final_cents);
+}
+
+bool operator ==(const Money &amount1, const Money &amount2) {
+  int get_cents1 = amount1.cents_ + (amount1.dollars_ * 100);
+  int get_cnets2 = amount2.cents_ + (amount2.dollars_ * 100);
+
+  if (get_cents1 == get_cnets2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const Money operator -(const Money &amount) {
+  return amount.cents_;
+}
+
+ostream& operator <<(ostream &out, const Money &amount) {
+  out << amount.cents_;
+}
+
+
+
+
+
+
+
+
+
+
+
+
