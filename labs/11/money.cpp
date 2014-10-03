c30beceb8936045e43d81db15d84c9ecfa25f1b8
Travis Peebles <cheadge@gmail.com>
2014-10-02 17:11:08 -0700

    Finished 11
    stream function look at the smart way to do it not the stupid one

--- labs/11/money.cpp
+++ labs/11/money.cpp
@@ -36,37 +36,61 @@ const Money operator -(const Money& amount1, const Money& amount2) {
   int final_cents = abs_all_cents % 100;
 
   if (sum_all_cents < 0) {
-    final_dollars -= final_dollars;
-    final_cents   -= final_cents;
+    final_dollars = -final_dollars;
+    final_cents  = -final_cents;
   }
-
   // return
   return Money(final_dollars, final_cents);
 }
 
 bool operator ==(const Money &amount1, const Money &amount2) {
-  int get_cents1 = amount1.cents_ + (amount1.dollars_ * 100);
-  int get_cnets2 = amount2.cents_ + (amount2.dollars_ * 100);
+  int get_cents1 = amount1.cents_ + amount1.dollars_ * 100;
+  int get_cnets2 = amount2.cents_ + amount2.dollars_ * 100;
   if (get_cents1 == get_cnets2) {
     return true;
   } else {
     return false;
   }
+  return true;
 }
 
 const Money operator -(const Money &amount) {
-  return amount.cents_;
+  // return the opposite of what it gets
+  return Money(-amount.dollars_, -amount.cents_);
 }
 
+/*
 ostream& operator <<(ostream &out, const Money &amount) {
-  int cents_1 = amount.cents_ + (amount.dollars_ * 100);
-  int abs_all_cents = abs(cents_1);
-  int final_dollars = abs_all_cents / 100;
-  int final_cents = abs_all_cents % 100;
-  out << "$" << final_dollars << "." << final_cents;
+  //  handle negatives
+  if (amount.cents_ < 0 || amount.dollars_ < 0) {
+    //  handle negatives that are greater then -10
+    if (amount.cents_ > -10) {
+      return out << "$-" << abs(amount.dollars_) << ".0" << abs(amount.cents_);
+    }
+    return out << "$-" << abs(amount.dollars_) << "." << abs(amount.cents_);
+  }
+  // handle less then 10
+  if (amount.cents_ < 10) {
+    return out << "$" << abs(amount.dollars_) << ".0" << abs(amount.cents_);
+  }
+  // straight print from object
+  return out << "$" << abs(amount.dollars_) << "." << abs(amount.cents_);
 }
+*/
 
-
+// USE THIS ONE NOT THE ONE ABOVE
+ostream& operator <<(ostream &out, const Money &amount) {
+  //  handle negatives
+  if (amount.dollars_ < 0 || amount.cents_ < 0) {
+    return out << "$-" << abs(amount.dollars_) << "."
+                    << setw(2) << setfill('0')
+                    << abs(amount.cents_);
+  } else {
+    return out << "$" << abs(amount.dollars_) << "."
+                    << setw(2) << setfill('0')
+                    << abs(amount.cents_);
+  }
+}
 
 
 
