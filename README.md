1f0c8f7ce90ff31495182aff973a27ea7c410d30
Travis Peebles <cheadge@gmail.com>
2014-10-21 11:44:39 -0700

    Lab 16 done, ignore and read me updated

--- README.md
+++ README.md
@@ -9,13 +9,17 @@ Labs, Assignments, and tools excluding examples
 | :----- | :------: | :----------  | :-------:|
 | Lab 1  |     Y    | Assignment 1 |     Y    |
 | Lab 2  |     Y    | Assignment 2 |     Y    |
-| Lab 3  |     Y    |
+| Lab 3  |     Y    | Assignment 3 |     N    |
 | Lab 4  |     Y    |
 | Lab 5  |     Y    |
 | Lab 6  |     Y    |
-| Lab 7  |     Y    | 
+| Lab 7  |     Y    |
 | Lab 8  |     Y    |
 | Lab 9  |     Y    |
 | Lab 10 |     Y    |
 | Lab 11 |     Y    |
-| Lab 12 |     Y    | 
+| Lab 12 |     Y    |
+| Lab 13 |     Y    |
+| Lab 14 |     Y    |
+| Lab 15 |     Y    |
+| Lab 16 |     Y    |
--- labs/16/lab_16.cpp
+++ labs/16/lab_16.cpp
@@ -1,6 +1,6 @@
 /*
  * Name        : lab_16.cpp
- * Author      : FILL IN
+ * Author      : Travis Peebles
  * Description : Working with a Dynamic Array in a Class
  */
 #include <iostream>
@@ -125,6 +125,56 @@ int main() {
 
 // CODE HERE -- FUNCTION DEFINITIONS
 
+ShoppingList::ShoppingList()
+  : items_(new string[25]),
+    max_items_(25),
+    item_count_(0) {
+    }
+
+ShoppingList::ShoppingList(unsigned int max_items)
+  : items_(new string[max_items]),
+    max_items_(max_items),
+    item_count_(0) {
+    }
+
+ShoppingList::~ShoppingList() {
+  delete[] items_;
+}
+
+unsigned int ShoppingList::max_items() const {
+  return max_items_;
+}
+
+unsigned int ShoppingList::item_count() const {
+  return item_count_;
+}
+
+bool ShoppingList::AddItem(string item) {
+  bool can_add = false;
+    if (item_count_ == max_items_) {
+      return can_add;
+    } else {
+      items_[item_count_] = item;
+      item_count_++;
+      can_add = true;
+    }
+  return can_add;
+}
+
+string ShoppingList::GetItem(unsigned int index) const {
+  return items_[index];
+}
+
+string& ShoppingList::GetItem(unsigned int index) {
+  return items_[index];
+}
+
+void ShoppingList::Reset(unsigned int max_items) {
+  delete[] items_;
+  max_items_ = max_items;
+  item_count_ = 0;
+  items_ = new string[max_items_];
+}
 
 // For testing (DO NOT ALTER)
 void UnitTest(int test) {
