601643f4f96632601c50c7d87c00af96aefbc35f
Travis Peebles <cheadge@gmail.com>
2014-10-30 22:42:06 -0700

    Lab 19 Done

---
+++ labs/19/sl_list.h
@@ -0,0 +1,39 @@
+/*
+ * Name        : sl_list.h
+ * Author      : Travis Peebles
+ * Description : class SLList.
+ *               Linked list content class
+ */
+
+#ifndef SLLIST_H
+#define SLLIST_H
+
+#include "sl_node.h"
+
+#include <sstream>
+#include <string>
+
+using std::string;
+using std::stringstream;
+
+class SLList {
+ public:
+  SLList();
+  ~SLList();
+  void InsertHead(int new_contents);
+  void InsertTail(int new_contents);
+  void RemoveHead();
+  void RemoveTail();
+  int GetHead() const;
+  int GetTail() const;
+  void Clear();
+  unsigned int size() const;
+  string ToString() const;
+
+ private:
+  SLNode* head_;
+  SLNode* tail_;
+  unsigned int size_;
+};
+
+#endif  // SLLIST_H
---
+++ labs/19/sl_node.cpp
@@ -0,0 +1,37 @@
+/*
+ * Name        : sl_node.cpp
+ * Author      : Travis Peebles
+ * Description : class SLNode.
+ *               Linked list function implementations
+ */
+
+#include "sl_node.h"
+
+SLNode::SLNode()
+  : next_node_(NULL),
+    contents_(0) {
+}
+
+SLNode::SLNode(int contents)
+  : next_node_(NULL),
+    contents_(contents) {
+}
+
+SLNode::~SLNode() {
+}
+
+void SLNode::set_contents(int contents) {
+  contents_ = contents;
+}
+
+int SLNode::contents() const {
+  return contents_;
+}
+
+void SLNode::set_next_node(SLNode* node) {
+  next_node_ = node;
+}
+
+SLNode* SLNode::next_node() const {
+  return next_node_;
+}
